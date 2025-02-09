8d0da7a67ad630fcd5a8b3aabcf8a9b108843e68
HarshTiwari6864 <harshtiwari6864@gmail.com>
2025-02-09 22:51:39 +0530

    Delete Backend/dist directory

--- Backend/dist/index.js
+++
@@ -1,9 +0,0 @@
-import app from "./app.js";
-import { connectToDatabase } from "./db/connection.js";
-const PORT = process.env.PORT || 5000;
-connectToDatabase().then(() => {
-    app.listen(PORT, () => (console.log("server is running and db is connected")));
-}).catch((error) => {
-    console.log(error);
-});
-//# sourceMappingURL=index.js.map
